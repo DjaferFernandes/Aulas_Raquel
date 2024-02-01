98a3e28ccd7bcd40f9cd8397e338636b594a1ae3
Djafer.Miguel <djafermiguel@gmail.com>
2024-02-01 15:35:32 -0300

    :)

--- Agenda.java
+++ Agenda.java
@@ -1,10 +1,55 @@
+import java.util.List;
 import java.util.Scanner;
-// Não ta adicionando mais de um contato :(
+// Ta adicionando :)
 public class Agenda {
     public static void main(String[] args) {
         //Scanner
         Scanner sc = new Scanner(System.in);
 
+        int inicializador = 5;
+        String[] nomes = new String[inicializador];
+        String[] email = new String[inicializador];
+        String[] numero = new String[inicializador];
+        int decisão;
+        int proxPosLivre = 0;
+        do {
+            System.out.println("Oq deseja fazer?");
+            decisão = Integer.parseInt(sc.nextLine());
+
+
+
+
+            if (decisão == 1) {
+                System.out.println("nome");
+                nomes[proxPosLivre] = sc.nextLine();
+                System.out.println("Email");
+                email[proxPosLivre] = sc.nextLine();
+                System.out.println("numero");
+                numero[proxPosLivre] = sc.nextLine();
+                decisão = 5;
+                proxPosLivre++;
+            }
+            if(decisão == 2){
+
+                System.out.println("Quer quem mzr?");
+                String p = sc.nextLine();
+
+                for (int i = 0; i < nomes.length; i++){
+
+                    if (String.valueOf(p).equals(String.valueOf(nomes[i]))) {
+                        System.out.println(nomes[i]);
+                        System.out.println(email[i]);
+                        System.out.println(numero[i]);
+                    }
+
+                }
+                decisão = 5;
+            }
+        } while (decisão == 5);
+
+
+
+        /*
         //Arrays
         int inicializador = 10;
         String[] nomes = new String[inicializador];
@@ -15,29 +60,33 @@ public class Agenda {
         int decisão = 0;
         while (decisão == 0){
             System.out.println("Oq deseja fazer?");
-            decisão = sc.nextInt();
-        }
-        while (decisão == 1) {
-            System.out.println("nome");
-            sc.next();
-            nomes[proxPosLivre] = sc.nextLine();
-            System.out.println("Email");
-            email[proxPosLivre] = sc.nextLine();
-            System.out.println("numero");
-            numero[proxPosLivre] = sc.nextLine();
-            proxPosLivre++;
-            decisão = 0;
-        }
-        // buscar contato
-        while (decisão == 2){
-            for (int i = 0; i < nomes.length; i++){
-                if (nomes[i] != null){
-                    System.out.println(nomes[i]);
-                }
+            decisão = Integer.parseInt(sc.nextLine());
+            if (decisão == 1) {
+                System.out.println("nome");
+                nomes[proxPosLivre] = sc.nextLine();
+                System.out.println("Email");
+                email[proxPosLivre] = sc.nextLine();
+                System.out.println("numero");
+                numero[proxPosLivre] = sc.nextLine();
+                proxPosLivre++;
+                decisão = 0;
             }
-            decisão = 0;
+            else if(decisão == 2){
+                for (int i = 0; i < nomes.length; i++){
+                    if (nomes[i] != null){
+                        System.out.println(nomes[i]);
+                    }
+                }
+                decisão = 0;
+            }
+            System.out.println("Deu ruim");
+            sc.close();
         }
-        System.out.println("Deu ruim");
-        sc.close();
+
+        // buscar contato
+
+
+         */
     }
+
 }
