0e9b4e1d6ffc4e1c2b1248c87ff026e2aaf0c800
djafer.miguel <djafermiguel@gmail.com>
2024-02-05 11:23:57 -0300

    :)

--- Aula17_ArrayList.java
+++ Aula17_ArrayList.java
@@ -1,3 +1,17 @@
-public class Aula17_ArrayList {
+import java.util.ArrayList;
 
+public class Aula17_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> food = new ArrayList<String>();
+        food.add("Pizza");
+        food.add("gabriel");
+        food.add("hot dog");
+        //food.set(0, "Sushi");
+        //food.remove(2);
+        //food.clear();
+
+        for(int i=0; i<food.size();i++){
+            System.out.println(food.get(i));
+        }
+    }
 }
---
+++ Aula18_2dArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Aula18_2dArrayList {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<String>> groceryList = new ArrayList();
+        ArrayList<String> bakeryList = new ArrayList();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+
+    }
+}
